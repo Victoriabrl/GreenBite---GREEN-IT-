425c922e63b43b5cbd50c6e81844762e2e5eb330
Hormone4 <117640003+Hormone4@users.noreply.github.com>
2025-04-13 14:49:58 +0200

    ProductsModule clearup

--- frontend/src/components/ProductsModule.vue
+++ frontend/src/components/ProductsModule.vue
@@ -8,22 +8,11 @@
         Sold by <a :href="'/#/vendors/show/'+ currentProduct.VendorID">{{ currentProduct.BusinessName }}</a>
       </p>
 
-      
       {{ currentProduct }}
-
     </div>
 
 
-    <!--
-    <div v-if="action === 'edit'">
-    </div>
-    -->
-    <!-- v-model is a two-way data-binding, when the input changes, the variable changes too -->
-
-
-
-    <!-- when on: /books/list/all -->
-    <div  v-if="action === 'list'">   <!-- v-if is a conditional rendering -->
+    <div  v-if="action === 'list'">
       <h1>Products List</h1>
 
       <ul class="products-list">
@@ -46,8 +35,6 @@
         </li>
           {{ productArray }}
       </ul>
-
-
     </div>
 
   </div>
@@ -58,41 +45,16 @@
   export default {
     name: 'Products',
     props: ['action', 'id'],  // properties that can be passed to the component
-    // action: show, edit, list
-    // id: book_id
+
     data() {
       return {   // variables that can be used in the template
-        //user_role: 'GUEST',
         pageNumber: 1,
         pageSize: 30,
         numberOfPages: 0,
   
         productArray: [],
-  
         currentProduct: {
-          /*[0] is game
-          */
-          /*
-          id: 0,
-          primary_name: "xxx",
-          description: "xxx",
-          yearpublished: 0,
-          minplayers: 0,
-          maxplayers: 0,
-          playingtime: 0,
-          minplaytime: 0,
-          maxplaytime: 0,
-          minage: 0,
-          bgg_rank: 0,
-          average_rating: 0.00,
-          bayes_average: 0.000,
-          users_rated : 0,
-          url: "xxx",
-          thumbnail: "xxx",
-          owned: 0,
-          trading: 0,
-          wanting: 0,
-          wishing: 0*/
+
         }
       };
     },
@@ -113,27 +75,7 @@
       async refreshcurrentProduct() {
         if (this.$props.id === "all" || this.$props.id === "0") {
           this.currentProduct = {
-            /*
-            id: 0,
-            primary_name: "xxx",
-            description: "xxx",
-            yearpublished: 0,
-            minplayers: 0,
-            maxplayers: 0,
-            playingtime: 0,
-            minplaytime: 0,
-            maxplaytime: 0,
-            minage: 0,
-            bgg_rank: 0,
-            average_rating: 0.00,
-            bayes_average: 0.000,
-            users_rated : 0,
-            url: "xxx",
-            thumbnail: "xxx",
-            owned: 0,
-            trading: 0,
-            wanting: 0,
-            wishing: 0*/
+            
           };
           return;
         }
@@ -168,7 +110,6 @@
     },
   
     created() {   // executed when the component is created
-      //this.getUserRole();
       this.getAllData();
       this.refreshcurrentProduct();
     }
